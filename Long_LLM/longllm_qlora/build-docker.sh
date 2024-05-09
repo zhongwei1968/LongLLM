d9051799c968b29435192f44112bf90f1b0eda40
Zhongwei Li <li_zhongwei@imda.gov.sg>
2024-05-09 13:08:36 +0800

    remove build file as contain ID info

--- Long_LLM/longllm_qlora/build-docker.sh
+++
@@ -1,9 +0,0 @@
-AWS_ACCOUNT_ID=052567997892
-#aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com
-#docker build --push . -t $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/llama-factory:latest
-#docker build . -t $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/llama-factory:latest
-
-AWS_ACCOUNT_ID=052567997892
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build . -t $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/ecr-aiss-gptx-longllm:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/ecr-aiss-gptx-longllm:latest
